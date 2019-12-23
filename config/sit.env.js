6931a478fe6ec1b40c15c9a6d3ebb6d4f2d71317
MJL <675531696@qq.com>
2019-12-23 16:25:30 +0800

    code

--- config/sit.env.js
+++ config/sit.env.js
@@ -4,7 +4,7 @@ module.exports = {
   // BASE_API: '',
   TITLE: '"管理系统（测试环境）"',
   BMOB: {
-    appid: '"e74e25be16b964ce7850299b3739a7bb"',
-    key: '"3a1e72a5546f810d9a1da0db98f17bac"'
+    appid: '"aaa"',
+    key: '"aaa"'
   }
 };
